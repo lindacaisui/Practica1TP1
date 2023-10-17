67a5a6b32a64abe88b4c6a9d75d4816477b53ca6
Nixo371 <nicolas.ucieda@gmail.com>
2023-10-17 18:53:41 +0200

    implementacion basica de RegularAlien

--- tp1/logic/gameobjects/RegularAlien.java
+++ tp1/logic/gameobjects/RegularAlien.java
@@ -4,6 +4,7 @@ import tp1.logic.AlienManager;
 import tp1.logic.Game;
 import tp1.logic.Move;
 import tp1.logic.Position;
+import tp1.view.Messages;
 
 /**
  * 
@@ -13,40 +14,135 @@ import tp1.logic.Position;
 public class RegularAlien {
 
 	//TODO fill your code
+	private static int ARMOR = 2;
+	private Position pos;
+	private int life;
+	private Game game;
+	private Move dir;
 	private int cyclesToMove;
 	private int speed;
-	private Move dir;
-	
 	private AlienManager alienManager;
 
-	//TODO fill your code
+	public RegularAlien(Position pos, Game game, AlienManager manager, int speed) {
+		this.pos = pos;
+		this.game = game;
+		this.alienManager = manager;
+		this.life = this.ARMOR;
+		this.dir = Move.LEFT;
+		this.speed = speed;
+		this.cyclesToMove = speed;
+	}
 
-	/**
-	 *  Implements the automatic movement of the regular alien	
-	 */
+	public boolean isAlive() {
+		return (life > 0);
+	}
+	
+	public int getLife() {
+		return (this.life);
+	}
+	
+	public void die() {
+
+	}
+	
+	public boolean isOnPosition(Position pos) {
+		return (this.pos.equals(pos));
+	}
+	
+	public Position getPosition() {
+		return (this.pos);
+	}
+	
+	public boolean receiveDamage(int damage) {
+		this.life -= damage;
+		return (life <= 0);
+	}
+	
+	public boolean isOut() {
+		return (this.pos.isOut());
+	}
+	
+	public boolean isInFinalRow() {
+		return (this.pos.get_fil() == Game.DIM_Y - 1);
+	}
+	
+	public void performMovement(Move dir) {
+		this.pos = this.pos.move(dir);		
+	}
+	
+	private String getSymbol() {
+		return (Messages.REGULAR_ALIEN_SYMBOL);
+	}
+	
+	public String toString() {
+		StringBuilder alien = new StringBuilder();
+		
+		alien.append(getSymbol());
+		alien.append('[');
+		alien.append(life);
+		alien.append(']');
+		
+		return (alien.toString());
+	}
+	
+	public String getInfo() {
+		StringBuilder info = new StringBuilder();
+		
+		info.append(this.getDescription());
+		
+		return (info.toString());
+	}
+	
+	private String getDescription() {
+		return (Messages.REGULAR_ALIEN_DESCRIPTION);
+	}
+	
+	public void computerAction() {
+		if (this.cyclesToMove == 0) {
+			this.automaticMove();
+			this.cyclesToMove = this.speed;
+		}
+		else {
+			this.cyclesToMove--;
+		}
+	}
+	
+	public void onDelete() {
+		
+	}
+	
 	public void automaticMove() {
-		//TODO fill your code
+		if (this.readyToDescend()) {
+			this.descent();
+			if (this.dir == Move.LEFT) { this.dir = Move.RIGHT; }
+			if (this.dir == Move.RIGHT) { this.dir = Move.LEFT; }
+		}
+		else {
+			this.performMovement(dir);
+		}
+	}
+	
+	private boolean readyToDescend() {
+		return (this.isInBorder() && this.pos.move(dir).isOut());
+	}
+	
+	private boolean isInBorder() {
+		return (this.pos.get_col() == 0 || this.pos.get_col() == Game.DIM_X - 1);
 	}
 
 	private void descent() {
-		//TODO fill your code
-		
+		this.performMovement(Move.DOWN);
 	}
-
-	private void performMovement(Move dir) {
-		//TODO fill your code
-		
-	}
-
-	private boolean isInBorder() {
-		//TODO fill your code
-		return false;
-	}
-
+	
 	public boolean receiveAttack(UCMLaser laser) {
-		//TODO fill your code
-		return false;
+		return (this.receiveDamage(laser.getDamage()));
 	}
+
+	
+
+	
+
+	
 	
 
 }
